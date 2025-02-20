6f25f2f14b34de6d7cfa56d3ca9858454645a4f8
Lawrence Forooghian <lawrence@forooghian.com>
2025-02-20 15:42:16 -0300

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -5,10 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "AblyCocoaLiveObjectsExample",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14),
+        .tvOS(.v14),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/ably/ably-cocoa",
+            branch: "2035-investigate-liveobjects-plugin"
+        ),
+        .package(
+            url: "https://github.com/lawrence-forooghian/ably-cocoa-liveobjects-plugin",
+            branch: "main"
+        ),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "AblyCocoaLiveObjectsExample"),
+            name: "AblyCocoaLiveObjectsExample",
+            dependencies: [
+                .product(
+                    name: "Ably",
+                    package: "ably-cocoa"
+                ),
+                .product(
+                    name: "AblyLiveObjects",
+                    package: "ably-cocoa-liveobjects-plugin"
+                ),
+            ]
+        ),
     ]
 )

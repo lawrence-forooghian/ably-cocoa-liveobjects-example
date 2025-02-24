350d665c6eb711305e9aa16f6cc68bdc6f171d57
Lawrence Forooghian <lawrence@forooghian.com>
2025-02-24 15:36:05 -0300

    add example of LiveObjects receiving some protocol messages

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,6 +3,7 @@ import AblyLiveObjects
 
 let clientOptions = ARTClientOptions(key: "INSERT API KEY HERE")
 clientOptions.liveObjectsPlugin = AblyLiveObjects.Plugin.self
+clientOptions.logLevel = .verbose
 
 let realtime = ARTRealtime(options: clientOptions)
 let channel = realtime.channels.get("foo")
@@ -13,8 +14,28 @@ print("got the channel")
 print(channel.liveObjects.someStruct)
 channel.liveObjects.doALiveObjectsThing()
 
+// Perform some channel activity so we can see that LiveObjects gets passed the protocol messages
+
+channel.attach { error in
+    if let error {
+        print("error attaching channel: \(error)")
+    } else {
+        print("attached channel")
+    }
+}
+
+channel.publish("myName", data: "myData") { error in
+    if let error {
+        print("error publishing message: \(error)")
+    } else {
+        print("published message")
+    }
+}
+
 // this is what we want to not work
 //channel.pluginDataValue(forKey: "hello")
 //let foo = PluginAPI.self
 //let foo: APLiveObjectsPluginProtocol
 //let bar: APLiveObjectsPluginFactoryProtocol
+
+RunLoop.main.run()
